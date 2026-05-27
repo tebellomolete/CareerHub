6a2b5e0e034ba74135def595193dbb38e4c25993
tebellomolete <ttmolete@gmail.com>
2026-05-27 09:32:08 +0200

    Configure native OpenAPI and Scalar UI in Program.cs

--- CareerHub.Api/Program.cs
+++ CareerHub.Api/Program.cs
@@ -1,23 +1,30 @@
+using Scalar.AspNetCore;
+using CareerHub.Api.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
+// 1. Enable Controllers
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+
+// 2. Add built-in OpenAPI generation
 builder.Services.AddOpenApi();
+
+// 3. Register our data store (we will create this next)
+builder.Services.AddSingleton<JobStore>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    
+    // 4. Map the interactive Scalar UI
+    app.MapScalarApiReference(); 
 }
 
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
+// 5. Map Controller routes
 app.MapControllers();
+
+// We will add Minimal API routes here later...
 
 app.Run();
