939f1fe68d16c648f7387fc8caaee9e5a8b4a516
tebellomolete <ttmolete@gmail.com>
2026-06-04 07:49:10 +0200

    touch ups

--- CareerHub.Api/Models/JobListing.cs
+++ CareerHub.Api/Models/JobListing.cs
@@ -1,6 +1,7 @@
 namespace CareerHub.Api.Models;
 
 public class JobListing
+{
     public Guid Id { get; set; }
     public string Title { get; set; } = string.Empty;
     public string Description { get; set; } = string.Empty;
